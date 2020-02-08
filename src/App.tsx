7015cf690d8d60ff3e61d202eedbe958b277cfc1
Kyle Miller <=>
2020-02-08 13:23:59 -0600

    initial commit for tab routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
+import { RoutedTabs, NavTab } from "react-router-tabs";
 import Header from './components/Header/Header';
 import DataEntry from './components/DataEntry/DataEntry';
+import './App.css';
+import 'react-router-tabs/styles/react-router-tabs.css';
 
 export default class App extends React.Component {
+
   render() {
     return (
       <div className="App">
         <Header />
-        <DataEntry />
-      </div>
+        <div className='navbar-wrapper'>
+          <NavTab to="/dataentry">Data Entry</NavTab>
+        </div>
+  
+        <Switch>
+          <Route
+            exact
+            path='/'
+            render={() => <Redirect to='/dataentry' />}
+          />
+          <Route path='/dataentry' component={DataEntry} />
+        </Switch>
+        </div>
     );
   }
 }
