732277711f8b1693102e2a1e69ac4f16f37fbcaa
Kyle Miller <=>
2020-02-10 07:48:01 -0600

    Use bar graph instead of line

--- src/components/Dashboard/DashboardView.tsx
+++ src/components/Dashboard/DashboardView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import './Dashboard.css';
 import { IDashboardViewProps } from './Dashboard.types';
@@ -10,7 +10,10 @@ const chartOptions = {
       scaleLabel: {
         display: true,
         labelString: 'Minutes'
-      }
+      },
+      ticks: {
+        beginAtZero: true
+    }
     }],
     xAxes: [{
       scaleLabel: {
@@ -30,30 +33,18 @@ export default class DashboardView extends React.Component<IDashboardViewProps, 
         datasets: [
           {
             label: title,
-            fill: false,
-            lineTension: 0.1,
             backgroundColor: 'rgba(75,192,192,0.4)',
             borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 4,
-            pointHitRadius: 10,
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(75,192,192,0.6)',
+            hoverBorderColor: 'rgba(75,192,192,1)',
             data: data
           }
         ]
       };
       return (
         <Col className='graph' xs={6} sm={6} md={6} lg={6}>
-            <Line
+            <Bar
                 data={graphData}
                 height={300}
                 options={chartOptions} />
